c6fa11317dddfd79a9379feee215ee55f305381e
MDylan <molnar.david@gmail.com>
2020-10-25 14:46:31 +0100

    Update img.php
    BMP hibajavítás

--- img.php
+++ img.php
@@ -1,28 +1,33 @@
 <?php
 
+set_time_limit(0);
+
 $filename = "image";
 
 if(file_exists($filename.".png")) {
     $im = imagecreatefrompng($filename.".png");
-    echo $filename.".png adatai:<br/>";
+    echo $filename.".png adatai:<br/>\n";
 } elseif(file_exists($filename.".jpg")) {
     $im = imagecreatefromjpeg ($filename.".jpg");
-    echo $filename.".jpg adatai:<br/>";
+    echo $filename.".jpg adatai:<br/>\n";
 } elseif(file_exists($filename.".jpeg")) {
     $im = imagecreatefromjpeg ($filename.".jpeg");
-    echo $filename.".jpeg adatai:<br/>";
+    echo $filename.".jpeg adatai:<br/>\n";
 } elseif(file_exists($filename.".bmp")) {
-    $im = imagecreatefromwbmp  ($filename.".bmp");
-    echo $filename.".bmp adatai:<br/>";
+    $im = imagecreatefrombmp ($filename.".bmp");
+	if($im === false) $im = imagecreatefromxbm($filename.".bmp");
+    echo $filename.".bmp adatai:<br/>\n";
 } else {
-    die("Nem létezik a kép!");
+    die("HIBA! Nem létezik a kép!\n");
 }
+
+if($im === false) die("Valami nem stimmel a képpel. Ha hibaüzenetet látsz, jelezd a fejlesztőnek.");
 
 $X = imagesx($im);
 $Y = imagesy($im);
 
-echo "A kép méretei: X:".$X." px Y:".$Y." px<br/>";
-
+echo "A kép méretei: X:".$X." px Y:".$Y." px<br/>\n";
+echo "Feldolgozás folyamatban, várj...\n\n";
 $fp = fopen('adatok.txt', 'w');
 $sor = "X;Y;R;G;B\n";
 fwrite($fp, $sor);
@@ -39,6 +44,6 @@ for($cx=0;$cx<$X;$cx++) {
 }
 fclose($fp);
 
-echo "<a href=\"adatok.txt\">Adatok letöltése</a>";
+echo "Kész! Az adatok.txt fájlban találod a részleteket. <a href=\"adatok.txt\">Adatok letöltése</a>";
 
 ?>
